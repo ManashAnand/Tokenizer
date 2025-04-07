6d6a997c843c2dacef1aeb402d5b2b526deca989
ManashAnand <anandmanash321@gmail.com>
2025-04-07 15:19:54 +0530

    added solana wallet

--- components/custom/Navbar.tsx
+++ components/custom/Navbar.tsx
@@ -37,47 +37,61 @@ const Navbar = () => {
     px-4 mt-4
 '>
             <div className=' flex-1 md:min-w-0 flex text-md md:text-2xl gap-2  items-center'><TowerControl className='' /> Tokenizer</div>
-            <div className='flex md:gap-4 text-xs min-w-3/4 md:min-w-0 justify-end '>
+            <div className='sm:flex hidden md:gap-4 text-xs min-w-3/4 md:min-w-0 justify-end '>
                 {
                     routes?.map((navItem, index) => {
                         return (
-                            <motion.div
-                                className={`px-2  md:px-4 py-2 rounded-md text-sm lg:text-base cursor-pointer relative no-underline duration-300 ease-in ${index == hoverIdx ? "text-white font-semibold" : "text-zinc-100"
-                                    }`}
-                                onHoverStart={() => setHoverIdx(index)}
-                                // onHoverEnd={() => setHoverIdx(0)}
-                                key={index}
+                            <div key={index}>
+                                <motion.div
+                                    className={`px-2  md:px-4 py-2 rounded-md text-sm lg:text-base cursor-pointer relative no-underline duration-300 ease-in ${index == hoverIdx ? "text-white font-semibold" : "text-zinc-100"
+                                        }`}
+                                    onHoverStart={() => setHoverIdx(index)}
+                                    // onHoverEnd={() => setHoverIdx(0)}
 
-                            >
-                                {
-                                    index == hoverIdx && (
-                                        <motion.div className="absolute bottom-0 left-0 h-full bg-green-800/40 rounded-md -z-10"
-                                            layoutId="navbar"
-                                            style={{
-                                                width: "100%",
-                                            }}
-                                            transition={{
-                                                type: "spring",
-                                                bounce: 0.25,
-                                                stiffness: 70,
-                                                damping: 9,
-                                                duration: 0.3,
-                                            }}
-                                        >
-                                        </motion.div>
-                                    )
-                                }
+                                >
+                                    {
+                                        index == hoverIdx && (
+                                            <motion.div className="absolute bottom-0 left-0 h-full bg-green-800/40 rounded-md -z-10"
+                                                layoutId="navbar"
+                                                style={{
+                                                    width: "100%",
+                                                }}
+                                                transition={{
+                                                    type: "spring",
+                                                    bounce: 0.25,
+                                                    stiffness: 70,
+                                                    damping: 9,
+                                                    duration: 0.3,
+                                                }}
+                                            >
+                                            </motion.div>
+                                        )
+                                    }
 
-                                {navItem.name}
-                            </motion.div>
+                                    {navItem.name}
+                                </motion.div>
+
+                            </div>
+
                         )
                     })
                 }
             </div>
-            <div className="flex gap-2 ml-2   relative z-50 isolate">
-                <WalletMultiButton className="!bg-transparent !hover:bg-green-800 !text-white !rounded-md !px-4 !py-2 !text-sm !font-semibold transition z-50 pointer-events-auto " />
-            </div>
-
+            <motion.div className="relative rounded-md text-white text-sm font-semibold ml-2">
+                <motion.div
+                    className="absolute bottom-0 left-0 h-full bg-green-800/20 rounded-md -z-10"
+                    layoutId="navbar"
+                    style={{ width: "100%" }}
+                    transition={{
+                        type: "spring",
+                        bounce: 0.25,
+                        stiffness: 70,
+                        damping: 9,
+                        duration: 0.3,
+                    }}
+                />
+                <WalletMultiButton className="!bg-transparent !text-white !px-4 !py-2 !text-sm !font-semibold relative z-10" />
+            </motion.div>
 
         </motion.div>
     )
